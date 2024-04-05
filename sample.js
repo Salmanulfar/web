a6d744a5b83793313e2679e3022c6007ff2e5b1e
Salmanulfar <salmanulfaris732002@gmail.com>
2024-04-05 14:51:43 +0530

    Salmanulfar

---
+++ sample.js
@@ -0,0 +1,25 @@
+function sendMail(){
+
+    var params={
+
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        message: document.getElementById("message").value
+    };
+
+
+const serviecID="service_8ybyham";
+const templateID="template_z4dc1by"
+
+emailjs.send(serviecID,templateID,params)
+.then(
+    (res)=>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("your message sent successfully")
+    }
+)
+.catch((err) => console.log(err));
+};
